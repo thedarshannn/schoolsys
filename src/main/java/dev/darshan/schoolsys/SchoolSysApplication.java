04ae8ab9b94162f95c9a21ec90a8b28cfd5336ca
Darshankumar Alpeshbhai Prajapati <mr.darshan2919@gmail.com>
2026-04-16 19:21:45 -0400

    Initial Commit.

---
+++ src/main/java/dev/darshan/schoolsys/SchoolSysApplication.java
@@ -0,0 +1,13 @@
+package dev.darshan.schoolsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolSysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolSysApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/darshan/schoolsys/SchoolSysApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.darshan.schoolsys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolSysApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
